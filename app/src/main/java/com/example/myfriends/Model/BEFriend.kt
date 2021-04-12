a10ce95a975301a153d70aa8c82b89b87fee7c4b
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2021-04-12 14:04:06 +0200

    Update BEFriend.kt

--- app/src/main/java/com/example/myfriends/Model/BEFriend.kt
+++ app/src/main/java/com/example/myfriends/Model/BEFriend.kt
@@ -1,4 +1,16 @@
 package com.example.myfriends.Model
 
-class BEFriend(var name: String, var phone: String, var isFavorite: Boolean ) {
+import java.util.*
+
+class BEFriend(
+    var id: Int,
+    var name: String,
+    var address: String,
+    var location: String,
+    var phone: String,
+    var mailAddress: String,
+    var website: String,
+    var birthday: Date,
+    var picture: String
+    ) {
 }
