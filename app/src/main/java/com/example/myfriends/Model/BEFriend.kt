482374a0d9a78ddae8aee08ee097a7f3738d8e22
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2021-04-12 14:05:46 +0200

    Revert "Update BEFriend.kt"
    This reverts commit a10ce95a975301a153d70aa8c82b89b87fee7c4b.

--- app/src/main/java/com/example/myfriends/Model/BEFriend.kt
+++ app/src/main/java/com/example/myfriends/Model/BEFriend.kt
@@ -1,16 +1,4 @@
 package com.example.myfriends.Model
 
-import java.util.*
-
-class BEFriend(
-    var id: Int,
-    var name: String,
-    var address: String,
-    var location: String,
-    var phone: String,
-    var mailAddress: String,
-    var website: String,
-    var birthday: Date,
-    var picture: String
-    ) {
+class BEFriend(var name: String, var phone: String, var isFavorite: Boolean ) {
 }
